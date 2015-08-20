e8b9ed1b949b63a368de1c1548f773b1c469b3a6
yecid villa <yecid.villa.ardila@gmail.com>
2015-08-20 20:16:13 +0200

    First Commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
@@ -12,4 +22,24 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)){
+        message("Getting cached Data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
+
+M <- matrix(nrow=3,ncol=3,data=c(1,7,5,9,2,3,11,13,8))
+M
+solve(M)
+
+y <- makeCacheMatrix(M)
+y
+z <- cacheSolve(y)
+z
+w <- cacheSolve(y)
+w
